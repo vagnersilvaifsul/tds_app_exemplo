18d62bc43068ca1fe57ae516159c92324cce52de
vagnersilvaifsul <vagnersilva@ifsul.edu.br>
2026-07-06 11:42:12 -0300

    tarefa1: ORM, inicialização do projeto e teste de execução na IDE Intellij.

---
+++ src/main/java/br/edu/ifsul/cstsi/tds_app_exemplo/TdsAppExemploApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsul.cstsi.tds_app_exemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdsAppExemploApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TdsAppExemploApplication.class, args);
+    }
+
+}
