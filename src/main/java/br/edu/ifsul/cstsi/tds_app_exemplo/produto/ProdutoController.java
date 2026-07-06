0abfb346db288142b40077abf8143cc3ab333c19
vagnersilvaifsul <vagnersilva@ifsul.edu.br>
2026-07-06 20:37:15 -0300

    tarefa3: injeção da dependência da repository e implementação da lógica de todos os métodos (sem DTO).

--- src/main/java/br/edu/ifsul/cstsi/tds_app_exemplo/produto/ProdutoController.java
+++ src/main/java/br/edu/ifsul/cstsi/tds_app_exemplo/produto/ProdutoController.java
@@ -3,6 +3,9 @@ package br.edu.ifsul.cstsi.tds_app_exemplo.produto;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.List;
 
 /*
     Pelo Princípio da Responsabilidade Única (SRP - Single-responsibility Principle) os controller manipulam
@@ -16,33 +19,71 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("api/v1/produtos") //Endpoint para toda classe
 public class ProdutoController {
 
+    private final ProdutoRepository produtoRepository;
+
+    //Realiza a injeção de dependência
+    public ProdutoController(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
+
     @GetMapping
-    public ResponseEntity<String> findAll(){
-        return ResponseEntity.ok("findAll");
+    public ResponseEntity<List<Produto>> findAll(){
+
+        return ResponseEntity.ok(produtoRepository.findAll().stream().toList());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<String> findById(@PathVariable Long id){
-        return ResponseEntity.ok("findById" + " " + id);
+    public ResponseEntity<Produto> findById(@PathVariable Long id){
+        var optionalProduto = produtoRepository.findById(id);
+        return optionalProduto.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @GetMapping("/nome/{nome}")
-    public ResponseEntity<String> findByNome(@PathVariable String nome){
-        return ResponseEntity.ok("findByNome" + nome);
+    public ResponseEntity<List<Produto>> findByNome(@PathVariable String nome){
+        var produtos = produtoRepository.findByNomeStartingWith(nome);
+        if (produtos.isPresent() && !produtos.get().isEmpty()) {
+            return ResponseEntity.ok(produtos.get());
+        }
+        return ResponseEntity.noContent().build();
     }
 
     @PostMapping
-    public ResponseEntity<String> insert(@RequestBody Produto produto){
-        return ResponseEntity.ok("insert" + " " + produto);
+    public ResponseEntity<String> insert(@RequestBody Produto produto, UriComponentsBuilder uriBuilder){
+        var p = produtoRepository.save(new Produto(
+                produto.getId(),
+                produto.getNome(),
+                produto.getValorDeCompra(),
+                produto.getValorDeVenda(),
+                produto.getDescricao(),
+                produto.getSituacao(),
+                produto.getEstoque()
+        ));
+        var location = uriBuilder.path("api/v1/produtos/{id}").buildAndExpand(p.getId()).toUri();
+        return ResponseEntity.created(location).build();
     }
 
     @PutMapping("{id}")
-    public ResponseEntity<String> update(@PathVariable Long id, @RequestBody Produto produto){
-        return ResponseEntity.ok("update" + " " + id + " " + produto);
+    public ResponseEntity<Produto> update(@PathVariable Long id, @RequestBody Produto produto){
+        var p = produtoRepository.save(new Produto(
+                id,
+                produto.getNome(),
+                produto.getValorDeCompra(),
+                produto.getValorDeVenda(),
+                produto.getDescricao(),
+                produto.getSituacao(),
+                produto.getEstoque()
+        ));
+        return p != null ?
+                ResponseEntity.ok(p) :
+                ResponseEntity.notFound().build();
     }
 
     @DeleteMapping("{id}")
     public ResponseEntity<String> delete(@PathVariable Long id){
-        return ResponseEntity.ok("delete" + " " + id);
+        if (produtoRepository.existsById(id)) {
+            produtoRepository.deleteById(id);
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.notFound().build();
     }
 }
