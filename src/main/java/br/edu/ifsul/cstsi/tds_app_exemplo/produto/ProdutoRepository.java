1747875e114a266c348355dfb9ffebfcb9aecac9
vagnersilvaifsul <vagnersilva@ifsul.edu.br>
2026-07-06 19:22:03 -0300

    tarefa3: adicionados métodos na repository deixar pronto para uso no controller.

--- src/main/java/br/edu/ifsul/cstsi/tds_app_exemplo/produto/ProdutoRepository.java
+++ src/main/java/br/edu/ifsul/cstsi/tds_app_exemplo/produto/ProdutoRepository.java
@@ -1,6 +1,33 @@
 package br.edu.ifsul.cstsi.tds_app_exemplo.produto;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+import java.util.Optional;
+
+/*
+    O Padrão Repository do Spring Data, apesar de nos fornecer uma inferface Domain Speak, inteligente o
+    suficiente para montar o SQL da consulta para nós em tempo de execução, é flexível o bastante para nos permitir
+    utilizar as nossas próprias Query Speak (é o caso do SQL apresentado na anotação @Query). Ou seja, o Spring Data nos
+    permite alternar entre Domain Speak e Query Speak rapidamente. É muita flexibilidade!
+ */
+
+//@RepositoryRestResource(exported = false) //se o sprig-rest-data estiver funcionando é preciso desligá-lo na Repository que você irá desenvolver
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+    /*
+        Para exemplificar eu coloquei a mesma busca em Domain Speak e Query Speak, só variei os nomes dos métodos.
+        Assim vocês podem contrastar as três diferentes maneiras de buscar dados no banco de dados com Spring Data.
+     */
+
+    //Método em Domain Speak (só pelo nome do método o Spring Data irá injetar o SQL em tempo de execução)
+    Optional<List<Produto>> findByNomeStartingWith(String nome);
+
+    //Método em Query Speak (em JPQL). Note o nome da entidade Produto no JPQL, isso mostra que ele roda no objeto JPA.
+    @Query("SELECT p FROM Produto p WHERE p.nome LIKE CONCAT(?1, '%')")
+    Optional<List<Produto>> findByNomeQuerySpeakJPQL(String nome);
+
+    //Método em Query Speak (em SQL). Note que utiliza o tradicional SQl, inclusive o nome da tabela é a que está no banco de dados.
+    @Query(value = "SELECT p.* FROM produtos p WHERE p.nome LIKE CONCAT(?1, '%') ORDER BY p.nome", nativeQuery = true)
+    Optional<List<Produto>> findByNomeQuerySpeakSQL(String nome);
 }
