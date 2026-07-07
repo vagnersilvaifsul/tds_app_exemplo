b5acad0bb286d318d71423fb6716278e93ab7f76
vagnersilvaifsul <vagnersilva@ifsul.edu.br>
2026-07-07 20:10:03 -0300

    tarefa4: fix, adição do JWT na documentação da API.

--- src/main/java/br/edu/ifsul/cstsi/tds_app_exemplo/infra/doc/SpringDocConfiguration.java
+++ src/main/java/br/edu/ifsul/cstsi/tds_app_exemplo/infra/doc/SpringDocConfiguration.java
@@ -1,25 +1,38 @@
 package br.edu.ifsul.cstsi.tds_app_exemplo.infra.doc;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration //indica que essa classe deve ser adicionada ao Contexto do aplicativo como um Bean de Configuração da Documentação da API
+@Configuration
+//indica que essa classe deve ser adicionada ao Contexto do aplicativo como um Bean de Configuração da Documentação da API
 public class SpringDocConfiguration {
     @Bean
     public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
         return new OpenAPI()
-            .info(new Info()
-                .title("API de Exemplo Desenvolvida para as Aulas de TDS")
-                .description("API Rest da aplicação tads_app_exemplo, contendo as funcionalidades, principalmente, para CRUD de produtos.")
-                .contact(new Contact()
-                    .name("Time Aulas TDS")
-                    .email("tds@ifsul.edu.br"))
-                .license(new License()
-                    .name("Apache 2.0")
-                    .url("http://localhost:8080/api/licenca")));
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .info(new Info()
+                        .title("API de Exemplo Desenvolvida para as Aulas de TDS")
+                        .description("API Rest da aplicação tds_app_exemplo, contendo todas as técnicas necessárias para trabalhar com Spring Boot.")
+                        .contact(new Contact()
+                                .name("Time Aulas TDS")
+                                .email("tads@ifsul.edu.br"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("http://localhost:8080/api/licenca")));
     }
 }
