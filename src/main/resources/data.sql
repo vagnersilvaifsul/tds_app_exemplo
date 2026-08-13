57834c9b8b347d46ddb474092cdf09961b6764dd
vagnersilvaifsul <vagnersilva@ifsul.edu.br>
2026-08-13 17:51:54 -0300

    didática: contexto na descrição de produtos

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 INSERT INTO produtos (id, nome, descricao, estoque, valor_de_compra, valor_de_venda, situacao)
 VALUES (1, 'Café', 'Café em pó tradicional Igaçu lata 400g', 100, 5.00, 10.00, true),
        (2, 'Erva Mate', 'Erva Mate Pura Folha 1kg', 100, 5.00, 10.00, true),
-       (3, 'Chá Preto', 'Prenda', 100, 5.00, 10.00, true),
+       (3, 'Chá Preto', 'Chá Preto Prenda cx 10 unidades', 100, 5.00, 10.00, true),
        (4, 'Arroz', 'Arroz Camil branco polido tipo 1 pacote 5kg', 100, 5.00, 10.00, true),
        (5, 'Feijão', 'Feijão Tordilho pacote 1kg', 100, 5.00, 10.00, true);
 
